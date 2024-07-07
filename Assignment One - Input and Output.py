67d2f3b6c8129956cfc3c5d993221e22e0f7c229
Tommy Luong <luong.tommy@gmail.com>
2024-07-06 17:41:25 -0700

    Initial check in for first assignment

---
+++ Assignment One - Input and Output.py
@@ -0,0 +1,20 @@
+"""This program prompts the user for the name and responds with a polite
+ greeting.
+"""
+
+
+def main():
+    """Prompt the user for their name and display a polite response."""
+    name = input("Please enter your name: ")
+    polite_response = "Hi " + name + ", let's explore some historical temperatures."
+    print(polite_response)
+
+
+if __name__ == "__main__":
+    main()
+
+"""
+---sample run #1---
+Please enter your name: Katie
+Hi Katie, let's explore some historical temperatures.
+"""
